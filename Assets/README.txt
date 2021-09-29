808f62422735ad7acd860158fe714256874eaa88
yasirkula <yasirkula@yahoo.com>
2021-09-29 09:35:27 +0300

    When an error is encountered, a popup will show up, informing the user about this error and prompting them to report it

---
+++ Assets/README.txt
@@ -0,0 +1,16 @@
+= Choice of Games Save Manager for Steam (Unofficial) =
+
+If you encounter any issues or have any suggestions, please don't hesitate
+to contact me via one of these mediums:
+
+- E-mail: yasirkula@gmail.com
+- Forum: https://forum.choiceofgames.com/t/save-manager-and-editor-for-steam-open-source/107649/
+- Reddit: https://www.reddit.com/user/yasirkula/
+- GitHub: https://github.com/yasirkula/UnityChoiceOfGamesSaveManager/issues
+
+This save manager is a hobby project of mine and I'm working on it alone,
+so there may be some bugs that slip my attention. When you encounter such
+bugs and report them to me, I'll try to fix them ASAP. While reporting bugs,
+please include the contents of "CoG Save Manager_Data/output_log.txt" with
+your bug report. Any errors encountered by the save manager are logged to
+that file and these error messages can help me debug the issues much faster.
---
+++ Assets/Scripts/ErrorReporter.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CoGSaveManager
+{
+	// Run this script before anything else
+	[DefaultExecutionOrder( -1000 )]
+	public class ErrorReporter : MonoBehaviour
+	{
+#pragma warning disable 0649
+		[SerializeField]
+		private GameObject errorPanel;
+
+		[SerializeField]
+		private InputField errorText;
+		private string displayedError;
+
+		[SerializeField]
+		private Button ignoreErrorButton, closeButton;
+#pragma warning restore 0649
+
+		private readonly Queue<string> queuedErrors = new Queue<string>( 16 );
+		private readonly HashSet<string> ignoredErrors = new HashSet<string>();
+		private readonly object queueLock = new object();
+
+		private void Awake()
+		{
+			Application.logMessageReceivedThreaded -= OnLogReceived;
+			Application.logMessageReceivedThreaded += OnLogReceived;
+
+			ignoreErrorButton.onClick.AddListener( () =>
+			{
+				if( !string.IsNullOrEmpty( displayedError ) )
+					ignoredErrors.Add( displayedError );
+
+				errorPanel.gameObject.SetActive( false );
+			} );
+
+			closeButton.onClick.AddListener( () => errorPanel.gameObject.SetActive( false ) );
+		}
+
+		private void OnLogReceived( string logString, string stackTrace, LogType logType )
+		{
+			if( logType == LogType.Error || logType == LogType.Exception )
+			{
+				string errorMessage = string.Concat( logString, "\n", stackTrace );
+				if( !ignoredErrors.Contains( errorMessage ) )
+				{
+					lock( queueLock )
+						queuedErrors.Enqueue( errorMessage );
+				}
+			}
+		}
+
+		private void LateUpdate()
+		{
+			if( queuedErrors.Count > 0 && !errorPanel.gameObject.activeSelf )
+			{
+				string error;
+				lock( queueLock )
+					error = queuedErrors.Dequeue();
+
+				if( ignoredErrors.Contains( error ) )
+					return;
+
+				errorText.text = displayedError = error;
+				errorPanel.gameObject.SetActive( true );
+			}
+		}
+	}
+}
